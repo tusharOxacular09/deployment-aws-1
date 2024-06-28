7c45d27d033f2a48e10591ce84edfe4fe376226e
Tushar Kanta Swain <tushar.swain@credencesoft.in>
2024-06-28 08:36:21 +0530

    Changesa

--- app.js
+++ app.js
@@ -7,4 +7,8 @@ app.get("/", (req, res) => {
   res.send("Hurray! I have successfully deployed this application on AWS.");
 });
 
+app.get("/health", (req, res) => {
+  res.send("My health is OK and what about your's!");
+});
+
 export default app;
